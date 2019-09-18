22bb251ba758f68dd795824c7cb16b1821468a7a
getraid-gg <getraid.gg@gmail.com>
2019-09-17 23:18:08 -0400

    Implement parenting for non-parentable IK targets

--- ECIKParentUnlocker/ECIKParentUnlocker.Hooks.cs
+++ ECIKParentUnlocker/ECIKParentUnlocker.Hooks.cs
@@ -1,5 +1,8 @@
 ﻿using Harmony;
 using HEdit;
+using Manager;
+using Pose;
+using RootMotion.FinalIK;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,13 +16,20 @@ using UnityEngine.UI;
 
 namespace ECIKParentUnlocker
 {
-    public static class Hooks
+    internal static class Hooks
     {
-
+        internal static readonly KinematicCtrl.IKTargetEN[] NonHardcodedIKTargets = new KinematicCtrl.IKTargetEN[]
+        {
+            KinematicCtrl.IKTargetEN.Body,
+            KinematicCtrl.IKTargetEN.LeftShoulder, KinematicCtrl.IKTargetEN.LeftArmChain,
+            KinematicCtrl.IKTargetEN.RightShoulder, KinematicCtrl.IKTargetEN.RightArmChain,
+            KinematicCtrl.IKTargetEN.LeftThigh, KinematicCtrl.IKTargetEN.LeftLegChain,
+            KinematicCtrl.IKTargetEN.RightThigh, KinematicCtrl.IKTargetEN.RightLegChain
+        };
     }
 
     [HarmonyPatch(typeof(MotionIKParentUI), "OpenCategory",
-    typeof(int), typeof(HPart.Group.MotionKind))]
+        typeof(int), typeof(HPart.Group.MotionKind))]
     internal static class OpenCategoryPatch
     {
         private static IEnumerable<CodeInstruction> Transpiler(ILGenerator generator, IEnumerable<CodeInstruction> instructions)
@@ -38,7 +48,7 @@ namespace ECIKParentUnlocker
             {
                 insertionPoint = instructionList.FindLastIndex((instruction) => instruction.opcode == OpCodes.Blt);
                 branchEndPoint = instructionList.FindIndex(insertionPoint, (instruction) => instruction.opcode == OpCodes.Callvirt
-                    && ((MethodInfo)instruction.operand).Equals(AccessTools.Property(typeof(TMPro.TMP_Text), "text").SetMethod));
+                    && ((MethodInfo)instruction.operand).Equals(AccessTools.Property(typeof(TMP_Text), "text").SetMethod));
             }
             catch (Exception e)
             {
@@ -88,7 +98,7 @@ namespace ECIKParentUnlocker
                 yield return new CodeInstruction(OpCodes.Sub);
                 yield return new CodeInstruction(OpCodes.Starg, 1);
 
-                var ikTargetIndexBuilder = generator.DeclareLocal(typeof(Pose.KinematicCtrl.IKTargetEN));
+                var ikTargetIndexBuilder = generator.DeclareLocal(typeof(KinematicCtrl.IKTargetEN));
                 ikTargetIndexBuilder.SetLocalSymInfo("ikTargetIndex");
                 yield return new CodeInstruction(OpCodes.Ldc_I4_M1);
                 yield return new CodeInstruction(OpCodes.Stloc, ikTargetIndexBuilder);
@@ -100,11 +110,6 @@ namespace ECIKParentUnlocker
 
                 // switch statement to get IK node ID
                 var jumpList = Enumerable.Range(0, 9).Select((i) => generator.DefineLabel()).ToArray();
-                var ikTargets = new Pose.KinematicCtrl.IKTargetEN[] { Pose.KinematicCtrl.IKTargetEN.Body,
-                    Pose.KinematicCtrl.IKTargetEN.LeftShoulder, Pose.KinematicCtrl.IKTargetEN.LeftArmChain,
-                    Pose.KinematicCtrl.IKTargetEN.RightShoulder, Pose.KinematicCtrl.IKTargetEN.RightArmChain,
-                    Pose.KinematicCtrl.IKTargetEN.LeftThigh, Pose.KinematicCtrl.IKTargetEN.LeftLegChain,
-                    Pose.KinematicCtrl.IKTargetEN.RightThigh, Pose.KinematicCtrl.IKTargetEN.RightLegChain };
                 var ikMenuTitles = new string[] { "Body IK Setting",
                     "Left shoulder IK Setting", "Left elbow IK Setting",
                     "Right shoulder IK Setting", "Right elbow IK Setting",
@@ -118,7 +123,7 @@ namespace ECIKParentUnlocker
 
                 for (int i = 0; i < jumpList.Length; i++)
                 {
-                    var loadTargetIndexInstruction = new CodeInstruction(OpCodes.Ldc_I4, (int)ikTargets[i]);
+                    var loadTargetIndexInstruction = new CodeInstruction(OpCodes.Ldc_I4, (int)Hooks.NonHardcodedIKTargets[i]);
                     loadTargetIndexInstruction.labels.Add(jumpList[i]);
                     yield return loadTargetIndexInstruction;
                     yield return new CodeInstruction(OpCodes.Stloc, ikTargetIndexBuilder);
@@ -140,7 +145,7 @@ namespace ECIKParentUnlocker
                 yield return new CodeInstruction(OpCodes.Ldarg_0);
                 yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(MotionIKParentUI), "textTitle"));
                 yield return new CodeInstruction(OpCodes.Ldloc, menuTitleBuilder);
-                yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Property(typeof(TMPro.TMP_Text), "text").SetMethod);
+                yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Property(typeof(TMP_Text), "text").SetMethod);
 
                 yield return new CodeInstruction(OpCodes.Br, branchEndLabel);
             }
@@ -182,43 +187,133 @@ namespace ECIKParentUnlocker
             MotionIKUI.IKUIInfo ___infoLeftShoulder, MotionIKUI.IKUIInfo ___infoLeftElbow,
             MotionIKUI.IKUIInfo ___infoRightShoulder, MotionIKUI.IKUIInfo ___infoRightElbow,
             MotionIKUI.IKUIInfo ___infoLeftWaist, MotionIKUI.IKUIInfo ___infoLeftKnee,
-            MotionIKUI.IKUIInfo ___infoRightWaist, MotionIKUI.IKUIInfo ___infoRightKnee,
-            HPart.Group.MotionKind ___kindMotion)
+            MotionIKUI.IKUIInfo ___infoRightWaist, MotionIKUI.IKUIInfo ___infoRightKnee)
         {
             var motionSettingCanvas = __instance.motionSettingCanvas;
             
             CopyIKParentingButton(___infoRightHand, ___infoBody, IKTargetIndices.Body,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoLeftShoulder, IKTargetIndices.LeftShoulder,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoLeftElbow, IKTargetIndices.LeftElbow,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoRightShoulder, IKTargetIndices.RightShoulder,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoRightElbow, IKTargetIndices.RightElbow,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoLeftWaist, IKTargetIndices.LeftWaist,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoLeftKnee, IKTargetIndices.LeftKnee,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoRightWaist, IKTargetIndices.RightWaist,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
             CopyIKParentingButton(___infoRightHand, ___infoRightKnee, IKTargetIndices.RightKnee,
-                motionSettingCanvas, ___kindMotion);
+                motionSettingCanvas, __instance);
         }
 
         private static void CopyIKParentingButton(MotionIKUI.IKUIInfo source, MotionIKUI.IKUIInfo target,
-            IKTargetIndices category, MotionSettingCanvas motionSettingCanvas, HPart.Group.MotionKind motionKind)
+            IKTargetIndices category, MotionSettingCanvas motionSettingCanvas, MotionIKUI uiObject)
         {
-            var ikButton = GameObject.Instantiate<GameObject>(source.btnIKSelectArea.gameObject).GetComponent<Button>();
+            var ikButton = UnityEngine.Object.Instantiate(source.btnIKSelectArea.gameObject).GetComponent<Button>();
             ikButton.transform.SetParent(target.tglUse.transform.parent, false);
-            ikButton.OnClickAsObservable().Subscribe(_ => motionSettingCanvas.motionIKParentUI.OpenCategory((int)category, motionKind));
+            // MotionIKUI.kindMotion is a private field, so the copycat function will have to use reflection :/
+            ikButton.OnClickAsObservable().Subscribe(_ => motionSettingCanvas.motionIKParentUI.OpenCategory((int)category,
+                (HPart.Group.MotionKind)AccessTools.Field(typeof(MotionIKUI), "kindMotion").GetValue(uiObject)));
             target.btnIKSelectArea = ikButton;
 
-            var buttonText = GameObject.Instantiate<GameObject>(source.textParent.gameObject).GetComponent<TextMeshProUGUI>();
+            var buttonText = UnityEngine.Object.Instantiate(source.textParent.gameObject).GetComponent<TextMeshProUGUI>();
             buttonText.transform.SetParent(target.btnIKSelectArea.transform, false);
             buttonText.text = "-";
             target.textParent = buttonText;
+        }
+    }
+
+    [HarmonyPatch(typeof(HEditGlobal), "SetIK",
+        typeof(ChaControl), typeof(Motion.IK), typeof(FullBodyBipedIK), typeof(KinematicCtrl))]
+    internal static class SetIKPatch
+    {
+        static void Postfix(HEditGlobal __instance, ChaControl _chara, Motion.IK _ik, KinematicCtrl _kinematicCtrl)
+        {
+            if (_chara == null || _ik == null || _kinematicCtrl == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < Hooks.NonHardcodedIKTargets.Length; i++)
+            {
+                int ikTargetIndex = (int)Hooks.NonHardcodedIKTargets[i];
+                var area = _ik.areas[ikTargetIndex];
+                var bone = _kinematicCtrl.lstIKBone[i];
+                // The IK targets not hardcoded to have IK parents don't have this method called for them
+                __instance.SetIKParent(_chara, _kinematicCtrl, area.parentCharaID, area.parentArea, ikTargetIndex);
+                // Then we need to call these to make sure everything is set up
+                __instance.SetIKPos(bone.changeAmount, area.amount);
+                bone.CalcTransform();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(HEditGlobal), "ReSetIKParent",
+        typeof(ChaControl), typeof(Motion.IK), typeof(KinematicCtrl))]
+    internal static class ReSetIKParentPatch
+    {
+        private static bool Prefix(HEditGlobal __instance, ChaControl _chara, Motion.IK _ik, KinematicCtrl _kinematicCtrl)
+        {
+            if (_chara == null || _ik == null || _kinematicCtrl == null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < 13; i++)
+            {
+                var area = _ik.areas[i];
+                __instance.SetIKParent(_chara, _kinematicCtrl, area.parentCharaID, area.parentArea, i);
+                __instance.SetIKPos(_kinematicCtrl.lstIKBone[i].changeAmount, area.amount);
+            }
+            return false;
+        }
+    }
+
+    // Yes, there's a typo in the original method's name.
+    [HarmonyPatch(typeof(MotionIKUI), "SetParetnName")]
+    internal static class SetParentNamePatch
+    {
+        private static void Postfix(MotionIKUI __instance,
+            MotionIKUI.IKUIInfo ___infoBody,
+            MotionIKUI.IKUIInfo ___infoLeftShoulder, MotionIKUI.IKUIInfo ___infoLeftElbow,
+            MotionIKUI.IKUIInfo ___infoRightShoulder, MotionIKUI.IKUIInfo ___infoRightElbow,
+            MotionIKUI.IKUIInfo ___infoLeftWaist, MotionIKUI.IKUIInfo ___infoLeftKnee,
+            MotionIKUI.IKUIInfo ___infoRightWaist, MotionIKUI.IKUIInfo ___infoRightKnee,
+            HPart.Group.MotionKind ___kindMotion)
+        {
+            if (__instance.CharaInfoData == null)
+            {
+                return;
+            }
+
+            var ik = __instance.CharaInfoData.motions[(int)___kindMotion].ik;
+
+            SetParentName(___infoBody, ik.areas[(int)KinematicCtrl.IKTargetEN.Body]);
+            SetParentName(___infoLeftShoulder, ik.areas[(int)KinematicCtrl.IKTargetEN.LeftShoulder]);
+            SetParentName(___infoLeftElbow, ik.areas[(int)KinematicCtrl.IKTargetEN.LeftArmChain]);
+            SetParentName(___infoRightShoulder, ik.areas[(int)KinematicCtrl.IKTargetEN.RightShoulder]);
+            SetParentName(___infoRightElbow, ik.areas[(int)KinematicCtrl.IKTargetEN.RightArmChain]);
+            SetParentName(___infoLeftWaist, ik.areas[(int)KinematicCtrl.IKTargetEN.LeftThigh]);
+            SetParentName(___infoLeftKnee, ik.areas[(int)KinematicCtrl.IKTargetEN.LeftLegChain]);
+            SetParentName(___infoRightWaist, ik.areas[(int)KinematicCtrl.IKTargetEN.RightThigh]);
+            SetParentName(___infoRightKnee, ik.areas[(int)KinematicCtrl.IKTargetEN.RightLegChain]);
+        }
+
+        private static void SetParentName(MotionIKUI.IKUIInfo targetUIInfo, Motion.IK.Area ikTarget)
+        {
+            if (ikTarget.parentArea != -1)
+            {
+                targetUIInfo.textParent.text = Singleton<HEditData>.Instance.dicIKSelectPrefabsInfos[ikTarget.parentArea].names[Singleton<GameSystem>.Instance.languageInt];
+            }
+            else
+            {
+                targetUIInfo.textParent.text = "-";
+            }
         }
     }
 }
